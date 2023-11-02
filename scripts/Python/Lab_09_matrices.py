98e505fddd35ad8ba8fbdf57f8fac328cbd7e0cd
Keith Lohse <lohse@wustl.edu>
2023-11-02 09:46:38 -0500

    Updates for lab 09
    Python and R scripts added for Lab 09 on matrices

--- scripts/Python/Lab_09_matrices.py
+++ scripts/Python/Lab_09_matrices.py
@@ -1,11 +1,16 @@
 import numpy as np
+import pandas as pd
 
 # 1a. Element-wise multiplication without loops
 dat = np.array([[16, 2, 3, 13],
                 [5, 11, 10, 8],
                 [9, 7, 6, 12],
                 [4, 14, 15, 1]])
+print(dat)
+
 vect = np.array([1, 2, 3, 4])
+
+print(vect)
 
 # Row-wise multiplication
 rowMultiplier = dat * vect[:, np.newaxis]
@@ -22,8 +27,17 @@ print(colMultiplier)
 # 1b. Vectorized alternative to replace loops
 data = -2 + 2 * np.random.rand(10000, 20000)
 
+# loop based approach
+for i in range(10000):
+    for j in range(20000):
+        if data[i, j] > 1:
+            data[i, j] = 0
+
 # Vectorized replacement of values greater than 1 with 0
 data[data > 1] = 0
+
+data.max()
+data.min()
 
 # 1c. Finding the maximum value without loops
 x = np.arange(1, 8)
@@ -33,6 +47,7 @@ X = np.tile(x, (len(y), 1))
 Y = np.tile(y[:, np.newaxis], (1, len(x)))
 
 maxVal = np.max(np.max(np.sin(X) * np.cos(Y)))
+print(maxVal)
 
 # Finding the location of the maximum value
 max_indices = np.unravel_index(np.argmax(np.sin(X) * np.cos(Y)), (len(y), len(x)))
--- scripts/R/lab_09_matrices.r
+++ scripts/R/lab_09_matrices.r
@@ -3,7 +3,10 @@ dat <- matrix(c(16, 2, 3, 13,
                 5, 11, 10, 8,
                 9, 7, 6, 12,
                 4, 14, 15, 1), nrow = 4, ncol = 4, byrow = TRUE)
+dat
+
 vect <- 1:4
+vect
 
 # Row-wise multiplication
 rowMultiplier <- dat * vect
@@ -21,6 +24,18 @@ print(colMultiplier)
 set.seed(123)  # Setting seed for reproducibility
 data <- -2 + 2 * matrix(runif(10000 * 20000), nrow = 10000, ncol = 20000)
 
+max(data)
+min(data)
+
+# loop-based solution
+for (i in 1:10000) {
+  for (j in 1:20000) {
+    if (data[i, j] > 1) {
+      data[i, j] <- 0
+    }
+  }
+}
+
 # Vectorized replacement of values greater than 1 with 0
 data[data > 1] <- 0
 
@@ -31,8 +46,13 @@ y <- 1:5
 X <- matrix(rep(x, each = length(y)), nrow = length(y), ncol = length(x), byrow = TRUE)
 Y <- matrix(rep(y, times = length(x)), nrow = length(y), ncol = length(x), byrow = TRUE)
 
-maxVal <- max(max(sin(X) * cos(Y))
+X
+Y
+
+maxVal <- max(max(sin(X) * cos(Y)))
 
 # Finding the location of the maximum value
 max_indices <- which(sin(X) * cos(Y) == maxVal, arr.ind = TRUE)
-cat(paste("The biggest element in XY is", maxVal, "at XY(", max_indices[1,], ",", max_indices[2,], ")\n"))
+max_indices
+
+cat(paste("The biggest element in XY is", maxVal, "at XY(", max_indices[1], ",", max_indices[2], ")\n"))
