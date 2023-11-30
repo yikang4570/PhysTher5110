0f9c1f4bdd5cb097b369628cbb382486289c7313
Keith Lohse <lohse@wustl.edu>
2023-11-30 12:47:25 -0600

    Update Lab_11_image_PCA.py

--- scripts/Python/Lab_11_image_PCA.py
+++ scripts/Python/Lab_11_image_PCA.py
@@ -1,94 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.io import imread
-from skimage.color import rgb2gray
+from sklearn.decomposition import PCA
+from skimage import io, color
 
-def pca_comparison(image_path):
-    # Load the image
-    quokka = imread(image_path)
+# Load the image
+image_path = "quokka.jpg"
+quokka_image = io.imread(image_path)
+
+# Convert the image to grayscale
+grayscale_quokka = color.rgb2gray(quokka_image)
+
+# Flatten the image to a 1D array
+flattened_quokka = grayscale_quokka.flatten()
+
+# Perform PCA
+n_components_list = [1, 20, 50, 100]
+for n_components in n_components_list:
+    pca = PCA(n_components=n_components)
+    reduced_quokka = pca.fit_transform(flattened_quokka.reshape(1, -1))
+    reconstructed_quokka = pca.inverse_transform(reduced_quokka)
+
+    # Reshape the reconstructed image to its original shape
+    reconstructed_quokka = reconstructed_quokka.reshape(grayscale_quokka.shape)
+
+    # Display the original and reconstructed images
+    plt.figure(figsize=(8, 4))
     
-    # Display the original image
-    plt.figure()
-    plt.imshow(quokka)
+    plt.subplot(1, 2, 1)
+    plt.imshow(grayscale_quokka, cmap='gray')
     plt.title('Original Image')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(reconstructed_quokka, cmap='gray')
+    plt.title(f'Reconstructed Image ({n_components} PCs)')
+
     plt.show()
 
-    # Convert the image to grayscale
-    quokka_bw = rgb2gray(quokka)
 
-    # Display the grayscale image
-    plt.figure()
-    plt.imshow(quokka_bw, cmap='gray')
-    plt.title('Grayscale Image')
-    plt.show()
-
-    # Get dimensions
-    M, N = quokka_bw.shape
-
-    # Subtract the mean for each dimension
-    data_zero_mean = quokka_bw - np.mean(quokka_bw)
-
-    # Calculate the covariance matrix
-    covariance = (1 / (N - 1)) * data_zero_mean @ data_zero_mean.T
-
-    # Find the eigenvectors and eigenvalues
-    Vpca, PCpca = np.linalg.eigh(covariance)
-
-    # Sort the variances in decreasing order
-    rindices = np.argsort(Vpca)[::-1]
-    Vpca = Vpca[rindices]
-    PCpca = PCpca[:, rindices]
-
-    # Project the original dataset
-    signals = PCpca.T @ data_zero_mean
-    # Recreate the original signal
-    org_pca = PCpca @ signals + np.mean(quokka_bw)
-
-    # Reconstruct the image using the first 10 PCs from PCA
-    num_components = 10
-    reconstruct_pca = PCpca[:, :num_components] @ signals[:num_components, :] + np.mean(quokka_bw)
-
-    # Perform PCA using singular value decomposition (SVD)
-    Y = data_zero_mean.T / np.sqrt(N - 1)
-
-    # Run the SVD
-    u, s, PCsvd = np.linalg.svd(Y, full_matrices=False)
-
-    # Calculate variances
-    Vsvd = s**2
-
-    # Project the original dataset
-    signals_svd = PCsvd @ data_zero_mean
-    # Recreate the original signal
-    org_svd = PCsvd.T @ signals_svd + np.mean(quokka_bw)
-
-    # Reconstruct the image using the first 10 PCs from SVD
-    reconstruct_svd = PCsvd[:, :num_components] @ signals_svd[:num_components, :] + np.mean(quokka_bw)
-
-    # Display the reconstructed images
-    plt.figure()
-    plt.imshow(reconstruct_pca, cmap='gray')
-    plt.title('Reconstructed Image (PCA)')
-    plt.show()
-
-    plt.figure()
-    plt.imshow(reconstruct_svd, cmap='gray')
-    plt.title('Reconstructed Image (SVD)')
-    plt.show()
-
-    # Use NumPy's built-in PCA function
-    _, _, Vpca_builtin = np.linalg.svd(data_zero_mean.T / np.sqrt(N - 1), full_matrices=False)
-
-    # Reconstruct the image using the first 10 PCs from built-in PCA
-    reconstruct_builtin = Vpca_builtin[:num_components, :] @ signals[:num_components, :] + np.mean(quokka_bw)
-
-    # Display the reconstructed image using NumPy's built-in PCA
-    plt.figure()
-    plt.imshow(reconstruct_builtin, cmap='gray')
-    plt.title('Reconstructed Image (Built-in PCA)')
-    plt.show()
-
-    return PCpca, Vpca, PCsvd, Vsvd
-
-# Call the function with the image path
-PCpca, Vpca, PCsvd, Vsvd = pca_comparison('quokka.jpg')
+    
